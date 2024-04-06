54127cf87c28b8ae2de8aad1e6462e5e0e7d60bf
IvoSa92 <ivo_sauter@web.de>
2024-04-06 09:03:03 +0200

    - added Ship Class:  - added function to receive a hit  - added function to calculate if the ship is sunk

---
+++ src/modules/ship.js
@@ -0,0 +1,18 @@
+class Ship {
+  constructor(name, length) {
+    this.name = name;
+    this.length = length;
+    this.hits = [];
+  }
+
+  hit(index) {
+    this.hits.push(index);
+  }
+
+  isSunk() {
+    if (this.hits.length === this.length.length) {
+      return true;
+    }
+    return false;
+  }
+}
