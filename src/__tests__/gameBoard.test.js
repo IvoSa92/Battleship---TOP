0dce64aff644a76e827712205b62a75116ff0a1e
IvoSa92 <ivo_sauter@web.de>
2024-04-06 11:25:17 +0200

    added gameboard.test.js: - added the tests for creating the board and placing the ships

---
+++ src/__tests__/gameBoard.test.js
@@ -0,0 +1,56 @@
+import Gameboard from "../modules/gameboard.js";
+import Ship from "../modules/ship.js";
+
+let board;
+
+beforeEach(() => {
+  board = new Gameboard(10);
+});
+
+test("board size is 10", () => {
+  expect(board.size).toBe(10);
+});
+
+test("every cell has the property ship=false", () => {
+  for (let i = 0; i < board.gameBoard.length; i++) {
+    board.gameBoard[i].forEach((cell) => {
+      expect(cell.ship).toBe(false);
+    });
+  }
+});
+
+test("every cell has the property hasBeenShot=false", () => {
+  for (let i = 0; i < board.gameBoard.length; i++) {
+    board.gameBoard[i].forEach((cell) => {
+      expect(cell.hasBeenShot).toBe(false);
+    });
+  }
+});
+
+test("gameboard has 100 cells", () => {
+  let count = 0;
+  for (let i = 0; i < board.gameBoard.length; i++) {
+    board.gameBoard[i].forEach((cell) => {
+      count++;
+    });
+  }
+  expect(count).toBe(100);
+});
+
+test("ship placing horizontal", () => {
+  const ship = new Ship("ivo", 4);
+  board.placeShip(ship, 0, 0, "horizontal");
+
+  for (let i = 0; i < ship.length; i++) {
+    expect(board.gameBoard[i][0].ship).toBe(true);
+  }
+});
+
+test("ship placing vertical", () => {
+  const ship = new Ship("ivo", 4);
+  board.placeShip(ship, 0, 0, "vertical");
+
+  for (let i = 0; i < ship.length; i++) {
+    expect(board.gameBoard[0][i].ship).toBe(true);
+  }
+});
--- src/index.js
+++ src/index.js
@@ -6,4 +6,5 @@ const board = new Gameboard(5);
 const ship = new Ship("destroyer", 3);
 board.placeShip(ship, 0, 0, "horizontal");
 
-console.log(ship.hits);
+console.log(board.gameBoard.length);
+console.log(board.gameBoard);
