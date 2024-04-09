1a50401343f936a9a501f59dc2f8f9a12408a16c
IvoSa92 <ivo_sauter@web.de>
2024-04-09 17:56:48 +0200

    HTML:
    - created the header part with title, logo and credits

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 import Gameboard from "./modules/gameboard.js";
 import Ship from "./modules/ship.js";
-
-const board = new Gameboard(5);
-
-const miri = new Ship("miri", 4);
-board.placeShip(miri, 0, 0, "horizontal");
-
-//console.log(board.gameBoard);
-console.log(miri);
