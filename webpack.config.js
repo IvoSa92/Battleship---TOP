1631996c3c08269b8970cb68cec0874e619bce4e
Ivo_Sa92 <141323736+IvoSa92@users.noreply.github.com>
2024-05-12 10:00:31 +0200

    bug fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  bundle: path.resolve(__dirname, "src/index.js"),
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
+
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
